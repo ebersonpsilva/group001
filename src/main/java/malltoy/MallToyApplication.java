b3885b0e11b65c67ec7398fe5a3ee8fd99d1c95a
Eberson Pergentino <ebersonpsilva@gmail.com>
2018-08-06 23:02:25 -0300

    Inclusão de projeto MAVEN SPRING e inclusão Layout

---
+++ src/main/java/malltoy/MallToyApplication.java
@@ -0,0 +1,12 @@
+package malltoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallToyApplication.class, args);
+	}
+}
